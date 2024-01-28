5ed1d0054acc1e29e561da2c3c8b5e4523429f45
endtnernoah <endtnernoah@gmail.com>
2024-01-28 16:03:24 +0100

    Added option to draw configurable amount of squares to the canvas;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,27 @@ import processing.core.PApplet;
 public class Main extends PApplet {
     private int WIDTH = 800;
     private int HEIGHT = 800;
+    private int CELL_SIZE = 8;
+
+    private int cellSpaceX = WIDTH / CELL_SIZE;
+    private int cellSpaceY = HEIGHT / CELL_SIZE;
+
+
+    private boolean[] cells = new boolean[cellSpaceX * cellSpaceY];
 
     public void settings(){
         size(WIDTH, HEIGHT);
     }
-    
+
     public void setup(){
-        size(800, 800);
-        noStroke();
-        background(200);
+        background(255);
+        for(int cellIndex = 0; cellIndex < cells.length; cellIndex++){
+            int cellX = cellIndex % cellSpaceX * CELL_SIZE;
+            int cellY = cellIndex / cellSpaceY * CELL_SIZE;
+
+            fill(255);
+            rect(cellX, cellY, CELL_SIZE, CELL_SIZE);
+        }
     }
 
     public void draw(){
