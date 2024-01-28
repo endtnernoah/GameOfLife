e41625594b55983567fd844a2dfdec629d986e43
endtnernoah <endtnernoah@gmail.com>
2024-01-28 16:10:18 +0100

    Setting up the starting generation randomly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import processing.core.PApplet;
 
+import java.util.Random;
+
 public class Main extends PApplet {
     private int WIDTH = 800;
     private int HEIGHT = 800;
@@ -9,25 +11,37 @@ public class Main extends PApplet {
     private int cellSpaceY = HEIGHT / CELL_SIZE;
 
 
-    private boolean[] cells = new boolean[cellSpaceX * cellSpaceY];
+    private int[] currentGeneration = new int[cellSpaceX * cellSpaceY];
+    private int[] nextGeneration = new int[cellSpaceX * cellSpaceY];
+
 
     public void settings(){
         size(WIDTH, HEIGHT);
+
+        // Setting up current generation with random numbers
+        for(int i = 0; i < currentGeneration.length; i++){
+            Random random = new Random();
+            currentGeneration[i] = random.nextInt(2);
+        }
     }
 
     public void setup(){
         background(255);
-        for(int cellIndex = 0; cellIndex < cells.length; cellIndex++){
-            int cellX = cellIndex % cellSpaceX * CELL_SIZE;
-            int cellY = cellIndex / cellSpaceY * CELL_SIZE;
 
-            fill(255);
-            rect(cellX, cellY, CELL_SIZE, CELL_SIZE);
-        }
     }
 
     public void draw(){
+        // Iterating over each cell to draw it
+        for(int cellIndex = 0; cellIndex < currentGeneration.length; cellIndex++){
+            int cellX = cellIndex % cellSpaceX * CELL_SIZE;
+            int cellY = cellIndex / cellSpaceY * CELL_SIZE;
 
+            System.out.println(currentGeneration[cellIndex]);
+
+            // Drawing each cell in the current generation
+            fill(255 * currentGeneration[cellIndex]);
+            rect(cellX, cellY, CELL_SIZE, CELL_SIZE);
+        }
     }
     public static void main(String[] args) {
         PApplet.main("Main");
